a7499a19f890d844feaf1af8f3eee0b904485f1b
KurnakovMaksim <maksk210@gmail.com>
2021-02-24 19:48:09 +0900

    Add repository interface

---
+++ NotSocialNetwork.Application/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotSocialNetwork.Application.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        T Add(T t);
+        T Update(T t);
+        T Delete(T t);
+        void Commit();
+    }
+}
